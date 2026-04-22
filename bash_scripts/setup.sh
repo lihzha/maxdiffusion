811e553b1b7b482c305a1d861bb39fbbe3e1d076
lihzha <lihanzha20@gmail.com>
2026-04-21 22:51:55 -0400

    remove

--- bash_scripts/setup.sh
+++ bash_scripts/setup.sh
@@ -1,7 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source ~/.zshrc
-cd maxdiffusion
-git checkout tenny-dev
 uv venv --python 3.12 ~/maxdiffusion_venv --seed
 source ~/maxdiffusion_venv/bin/activate
 bash setup.sh MODE=stable DEVICE=tpu
