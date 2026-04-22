d26b1323ff0a7454bfe1a0c4fe8d9c4e10671b13
lihzha <lihanzha20@gmail.com>
2026-04-22 09:01:51 -0400

    rm

--- bash_scripts/setup.sh
+++ bash_scripts/setup.sh
@@ -1,6 +1,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source ~/.zshrc
-rm -rf ~/maxdiffusion_venv
 uv venv --python 3.12 ~/maxdiffusion_venv --seed
 source ~/maxdiffusion_venv/bin/activate
 bash setup.sh MODE=stable DEVICE=tpu
