db2b56e6ea24a0067f0a8a5807cdfce9fff658bc
Catherine Zheng <zhonghezheng13579@gmail.com>
2026-05-11 15:27:31 -0400

    training for wan model

--- src/maxdiffusion/train_wan.py
+++ src/maxdiffusion/train_wan.py
@@ -30,6 +30,9 @@ def train(config):
   if config.model_type == "I2V":
     from maxdiffusion.trainers.wan_i2v_trainer import WanI2VTrainer
     trainer = WanI2VTrainer(config)
+  elif config.model_type == "AC_TI2V":
+    from maxdiffusion.trainers.wan_ctrl_world_trainer import WanCtrlWorldTrainer
+    trainer = WanCtrlWorldTrainer(config)
   else:
     from maxdiffusion.trainers.wan_trainer import WanTrainer
     trainer = WanTrainer(config)
