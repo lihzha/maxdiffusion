5648ed3079df33a86288cce350af645f1f5ffb79
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-05-14 21:24:14 -0400

    fixed some training error

--- src/maxdiffusion/checkpointing/wan_checkpointer.py
+++ src/maxdiffusion/checkpointing/wan_checkpointer.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import os
 from abc import ABC, abstractmethod
 from typing import Optional, Tuple
 from maxdiffusion.checkpointing.checkpointing_utils import (create_orbax_checkpoint_manager)
@@ -35,8 +36,9 @@ class WanCheckpointer(ABC):
     self.checkpoint_type = checkpoint_type
     self.opt_state = None
 
+    checkpoint_dir = os.path.abspath(self.config.checkpoint_dir)
     self.checkpoint_manager: ocp.CheckpointManager = create_orbax_checkpoint_manager(
-        self.config.checkpoint_dir,
+        checkpoint_dir,
         enable_checkpointing=True,
         save_interval_steps=1,
         checkpoint_type=checkpoint_type,
