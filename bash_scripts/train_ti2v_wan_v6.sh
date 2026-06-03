f9bea67633ad08f62a0abf1d26af457ef3e135bf
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-03 14:38:54 -0400

    modified training script

--- bash_scripts/train_ti2v_wan_v6.sh
+++ bash_scripts/train_ti2v_wan_v6.sh
@@ -115,9 +115,9 @@ python src/maxdiffusion/train_wan.py \
     activations_dtype=bfloat16 \
     remat_policy=FULL \
     ici_data_parallelism=2 \
-    ici_fsdp_parallelism=4 \
+    ici_fsdp_parallelism=2 \
     ici_tensor_parallelism=1 \
-    ici_context_parallelism=4 \
+    ici_context_parallelism=8 \
     dcn_data_parallelism=1 \
     dcn_fsdp_parallelism=1 \
     dcn_tensor_parallelism=1 \
