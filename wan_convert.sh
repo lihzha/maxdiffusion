7eb8a724a08cad7fe5a415980dfebfa93b73c636
Zhonghe Zheng <zz8976@neuronic.cs.princeton.edu>
2026-06-04 16:30:51 -0400

    test lower_res

--- wan_convert.sh
+++ wan_convert.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=150G
-#SBATCH --time=24:00:00
+#SBATCH --time=60:00:00
 #SBATCH --output=slurm_outputs/%x/out_%x_%A_%a.out
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=zz8976@princeton.edu
@@ -33,8 +33,8 @@ CHUNK=$(( ( (_RAW_CHUNK + NO_RECORDS_PER_SHARD - 1) / NO_RECORDS_PER_SHARD ) * N
 START=$(( SLURM_ARRAY_TASK_ID * CHUNK ))
 
 # Last job encodes everything from its start to end of dataset.
-# MAX_EP=${CHUNK}
-MAX_EP=2000
+MAX_EP=${CHUNK}
+
 if [ "${SLURM_ARRAY_TASK_ID}" = "$(( N_JOBS - 1 ))" ]; then
     MAX_EP=-1
 fi
@@ -43,15 +43,15 @@ python src/maxdiffusion/data_preprocessing/wan2.2_txt2vid_data_preprocessing.py 
     src/maxdiffusion/configs/base_wan_ctrl_world.yml \
     pretrained_model_name_or_path=model/Wan2.2-TI2V-5B-Diffusers \
     raw_data_root=/n/fs/iromdata/droid_raw/1.0.1 \
-    data_root=/n/fs/iromdata/droid_wan2.2 \
+    data_root=/n/fs/iromdata/droid_wan2.2_lowres \
     video_index_path=/n/fs/iromdata/droid_wan2.2/video_index.json \
     action_stats_path=/n/fs/iromdata/droid_wan2.2/stats.json \
     no_records_per_shard=${NO_RECORDS_PER_SHARD} \
     max_frames=300 \
     max_episodes=${MAX_EP} \
-    start_episode=$((START + 7210)) \
-    height=704 \
-    width=1280 \
+    start_episode=${START} \
+    height=480 \
+    width=832 \
     hardware=gpu \
     val_fraction=0.05
 
@@ -61,13 +61,13 @@ python src/maxdiffusion/data_preprocessing/wan2.2_txt2vid_data_preprocessing.py 
 #     pretrained_model_name_or_path=model/Wan2.2-TI2V-5B-Diffusers \
 #     raw_data_root=/n/fs/iromdata/droid_raw/1.0.1 \
 #     data_root=droid_wan_tfrecords_test \
-#     video_index_path=droid_wan_tfrecords_test/video_index.json \
-#     no_records_per_shard=10 \
+#     video_index_path=/n/fs/iromdata/droid_wan2.2/video_index.json \
 #     action_stats_path=/n/fs/iromdata/droid_wan2.2/stats.json \
+#     no_records_per_shard=10 \
 #     max_frames=300 \
 #     max_episodes=210 \
 #     start_episode=0 \
-#     height=704 \
-#     width=1280 \
+#     height=480 \
+#     width=832 \
 #     hardware=gpu \
 #     val_fraction=0.05
