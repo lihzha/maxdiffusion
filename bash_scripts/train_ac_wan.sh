75c0f4a4acdaa4bbcbaf631a21171e0f5a06866e
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-08 03:53:41 -0400

    moved wandb download

--- bash_scripts/train_ac_wan.sh
+++ bash_scripts/train_ac_wan.sh
@@ -131,6 +131,7 @@ python src/maxdiffusion/train_wan.py \
     scan_layers=True \
     max_train_steps=100000 \
     checkpoint_every=100 \
+    checkpoint_keep_period=10000 \
     per_device_batch_size=0.25 \
     height=480 \
     width=832 \
