5c1b273714b78b26223a85b6626cde65ff9745f5
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-11 17:46:31 -0400

    config

--- bash_scripts/train_ti2v_wan_self_distillation.sh
+++ bash_scripts/train_ti2v_wan_self_distillation.sh
@@ -128,9 +128,9 @@ python src/maxdiffusion/train_wan.py \
     dcn_context_parallelism=1 \
     allow_split_physical_axes=True \
     scan_layers=True \
-    max_train_steps=10001 \
+    max_train_steps=1001 \
     checkpoint_every=10 \
-    checkpoint_keep_period=1000 \
+    checkpoint_keep_period=100 \
     per_device_batch_size=0.25 \
     height=480 \
     width=832 \
