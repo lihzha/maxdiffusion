367e1eec3a0111cba9c92375f3da6ac7c868af1e
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-14 22:08:09 -0400

    config

--- bash_scripts/train_ti2v_wan_self_distillation.sh
+++ bash_scripts/train_ti2v_wan_self_distillation.sh
@@ -106,7 +106,7 @@ export LIBTPU_INIT_ARGS='--xla_tpu_enable_async_collective_fusion_fuse_all_gathe
 XLA_PYTHON_CLIENT_MEM_FRACTION=0.95 \
 python src/maxdiffusion/train_wan.py \
     src/maxdiffusion/configs/base_wan_5b.yml \
-    run_name=ti2v_wan_droid_self_distill_single_view \
+    run_name=ti2v_wan_droid_self_distill_multi_view \
     output_dir=gs://v6_east1d/checkpoints/wan-ti2v-finetune \
     pretrained_model_name_or_path=$WAN_TI2V_MODEL_DIR \
     dataset_type=tfrecord \
@@ -142,8 +142,8 @@ python src/maxdiffusion/train_wan.py \
     teacher_update_every=50 \
     distill=True \
     oracle_noise_offset=-1 \
-    wandb_project='wan-ti2v-self-distill-clean-single-view' \
-    single_camera=True 
+    wandb_project='wan-ti2v-self-distill-clean-multi-view' \
+    single_camera=False 
 
 # --- 6. Unmount ---
 fusermount -u "$GCS_MOUNT" || fusermount -uz "$GCS_MOUNT"
