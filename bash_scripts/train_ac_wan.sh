a2bcfc522b842a9bc64a111c34979b9dc59ae9d0
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-19 01:41:21 -0400

    config

--- bash_scripts/train_ac_wan.sh
+++ bash_scripts/train_ac_wan.sh
@@ -110,11 +110,11 @@ python src/maxdiffusion/train_wan.py \
     attention=flash \
     weights_dtype=bfloat16 \
     activations_dtype=bfloat16 \
-    remat_policy=OFFLOAD_MATMUL_WITHOUT_BATCH \
-    ici_data_parallelism=8 \
+    remat_policy=FULL \
+    ici_data_parallelism=16 \
     ici_fsdp_parallelism=4 \
     ici_tensor_parallelism=1 \
-    ici_context_parallelism=2 \
+    ici_context_parallelism=1 \
     dcn_data_parallelism=1 \
     dcn_fsdp_parallelism=1 \
     dcn_tensor_parallelism=1 \
@@ -139,6 +139,15 @@ python src/maxdiffusion/train_wan.py \
     # ici_tensor_parallelism=1 \
     # ici_context_parallelism=2 \
     # per_device_batch_size=1.0 \
+
+    # 9.89 s/step
+    # remat_policy=OFFLOAD_MATMUL_WITHOUT_BATCH \
+    # ici_data_parallelism=8 \
+    # ici_fsdp_parallelism=4 \
+    # ici_tensor_parallelism=1 \
+    # ici_context_parallelism=2 \
+    # per_device_batch_size=1.0 \
+
 
 
 # --- DROID path (uncomment to use) ---
