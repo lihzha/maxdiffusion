43a8be49fdbed577bf2179e4ddd5d7cd1d7d6e9a
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-21 19:26:01 -0400

    config

--- bash_scripts/train_ac_wan.sh
+++ bash_scripts/train_ac_wan.sh
@@ -126,6 +126,7 @@ python src/maxdiffusion/train_wan.py \
     max_train_steps=100100 \
     checkpoint_every=100 \
     checkpoint_keep_period=10000 \
+    eval_every=50 \
     height=480 \
     width=832 \
     num_frames=81 \
