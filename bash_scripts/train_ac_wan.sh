53911b6da31fb4942edab613c10ba2885fe61d99
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-06-23 00:48:49 -0400

    config

--- bash_scripts/train_ac_wan.sh
+++ bash_scripts/train_ac_wan.sh
@@ -107,10 +107,10 @@ python src/maxdiffusion/train_wan.py \
     eval_data_dir=gs://v6_east1d/wan2.2_tfr_dataset_lowres/val \
     action_stats_path=gs://v6_east1d/wan2.2_tfr_dataset_lowres/stats.json \
     cache_latents_text_encoder_outputs=True \
-    attention=flash \
+    attention=tokamax_flash \
     weights_dtype=bfloat16 \
     activations_dtype=bfloat16 \
-    remat_policy=MATMUL_WITHOUT_BATCH \
+    remat_policy=HIDDEN_STATE_WITH_OFFLOAD \
     ici_data_parallelism=1 \
     ici_fsdp_parallelism=-1 \
     ici_tensor_parallelism=1 \
@@ -122,9 +122,9 @@ python src/maxdiffusion/train_wan.py \
     per_device_batch_size=1.0 \
     grad_accum_steps=1 \
     allow_split_physical_axes=True \
-    scan_layers=True \
-    max_train_steps=100100 \
-    checkpoint_every=100 \
+    scan_layers=False \
+    max_train_steps=101000 \
+    checkpoint_every=1000 \
     checkpoint_keep_period=10000 \
     eval_every=50 \
     height=480 \
