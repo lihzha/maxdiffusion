085921a9bcfd2a6329e44a55092d599a639cb02a
Zhonghe Zheng <zz8976@della-gpu.princeton.edu>
2026-07-11 01:03:55 -0400

    config

--- bash_scripts/train_ac_wan.sh
+++ bash_scripts/train_ac_wan.sh
@@ -114,7 +114,7 @@ python src/maxdiffusion/train_wan.py \
     attention=tokamax_flash \
     weights_dtype=float32 \
     activations_dtype=bfloat16 \
-    remat_policy=HIDDEN_STATE_WITH_OFFLOAD \
+    remat_policy=MATMUL_WITHOUT_BATCH \
     ici_data_parallelism=1 \
     ici_fsdp_parallelism=-1 \
     ici_tensor_parallelism=1 \
